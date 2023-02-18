2d40d0390c6cc1b98aeb60a3a40531b44d0b4713
sisiayang <sisia1329@gmail.com>
2023-02-18 15:14:09 +0800

    整理first layer model

---
+++ first_layer_prepare_data.py
@@ -0,0 +1,62 @@
+import torch
+import networkx as nx
+from torch.utils.data import Dataset, DataLoader
+
+class graphDataset(Dataset):
+    def __init__(self, graph_list, label_list):
+        feature_matrix_list = []
+        for g in graph_list:
+            feature_matrix = [list(g.nodes[j].values()) for j in range(1, g.number_of_nodes()+1)]
+            feature_matrix_list.append(torch.tensor(feature_matrix, dtype=torch.float))
+        
+        self.feature_matrix = feature_matrix_list
+        
+        adj_matrix_list = []
+        for g in graph_list:
+            adj_matrix = nx.adjacency_matrix(g).todense()
+            adj_matrix_list.append(torch.tensor(adj_matrix, dtype=torch.float))
+        self.adj_matrix = adj_matrix_list
+
+        labels = []
+        for i in range(len(label_list)):
+            labels.append(torch.tensor(label_list[i], dtype=torch.int64))
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        features = self.feature_matrix[idx]
+        adj_matrix = self.adj_matrix[idx]
+        labels = self.labels[idx]
+
+        return features, adj_matrix, labels
+
+def create_graph(df):  # df size = graph size
+    size = len(df)+1
+    G = nx.Graph()
+    nodes = range(1, size)
+    G.add_nodes_from(nodes)
+    edge_list = []
+    for uid in range(1, size):
+        neighbor_list = df[df['uid'] == uid].iloc[0]['linked'].split(',')
+        edge_list = [(int(uid), int(neighbor)) for neighbor in neighbor_list]
+        G.add_edges_from(edge_list)
+        G.add_edge(int(uid), int(uid))
+    return G
+
+def add_feature(graph, f_df):
+    feature_col_list = ['color_red', 'color_black', 'color_green', 'color_blue', 'score', 'num_of_neighbor', 'hist_color', 'hist_neighbor', 'hist_skip']
+    for n in graph.nodes():
+        for f in feature_col_list:
+            graph.nodes[n][f] = f_df[f_df['uid']==n][f].tolist()[0]    # 取出uid符合的user的f欄位，從series型態轉成list再取值
+    return graph
+
+def create_dataloader(graph_list, label_list):
+    train_dataset = graphDataset(graph_list[:90], label_list[:90])
+    test_dataset = graphDataset(graph_list[90:], label_list[90:])
+
+    train_dataloader = DataLoader(dataset=train_dataset, batch_size=1, shuffle=True)
+    test_dataloader = DataLoader(dataset=test_dataset, batch_size=1)
+
+    return train_dataloader, test_dataloader
---
+++ first_layer_train.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+import time
+import torch
+from first_layer_prepare_data import create_graph, add_feature, create_dataloader
+from first_layer_model import GCN_Model
+
+def accuracy(output, labels):
+    preds = output.max(1)[1].type_as(labels)
+    correct = preds.eq(labels).double().sum()
+    return np.round(correct / len(labels), 2)
+
+def train(model, train_dataloader, test_dataloader, optimizer, loss_fn, epoch, eval=True):
+    f = open('Output/action_predict_result.txt', 'w')
+
+    t = time.time()
+    loss_hist = []
+    for i in range(epoch):
+        model.train()
+        for features, adj_matrix, labels in iter(train_dataloader):
+            features = features.squeeze()
+            adj_matrix = adj_matrix.squeeze()
+            labels = labels.squeeze()
+            
+            output = model(features, adj_matrix)
+
+            loss = loss_fn(output, labels)
+            loss_hist.append(float(loss))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        
+        if(eval):
+            model.eval()
+            total_loss = 0
+            for features, adj_matrix, labels in iter(test_dataloader):
+                features = features.squeeze()
+                adj_matrix = adj_matrix.squeeze()
+                labels = labels.squeeze()
+                output = model(features, adj_matrix)
+                total_loss += float(loss_fn(output, labels))
+            
+            f.write('eval avg loss: ' + str(total_loss/len(test_dataloader)) + '\n')
+            f.write('acc: ' + str(float(accuracy(output, labels))) + '\n')
+            f.write('='*80 + '\n\n')
+
+    f.close()
+    return loss_hist
+
+
+if __name__ == '__main__':
+    feature_df = pd.read_csv('data/graph_feature.csv')
+    structure_df = pd.read_csv('data/graph_structure.csv')
+
+    col = feature_df.columns.tolist()
+    col.remove('color')
+    new_col = ['color_red', 'color_black', 'color_green', 'color_blue'] + col
+
+
+
+    ct = ColumnTransformer([('color', OneHotEncoder(), [3])], remainder='passthrough')
+    feature_onehot = np.array(ct.fit_transform(feature_df))
+    feature_onehot_df = pd.DataFrame(feature_onehot, columns=new_col)
+
+    graph_list = []
+    label_list = []
+    for session in range(1, 14):
+        s_all_df = structure_df[structure_df['session'] == session]
+        for round in s_all_df['round'].unique():
+            
+            s_df = s_all_df[s_all_df['round'] == round]
+            f_df = feature_onehot_df.iloc[s_df.index]
+            
+            graph = create_graph(s_df)
+            graph = add_feature(graph, f_df)
+            graph_list.append(graph)
+
+            labels = f_df['action'].tolist()
+            label_list.append(labels)
+
+    train_dataloader, test_dataloader = create_dataloader(graph_list, label_list)
+    model = GCN_Model(9, 9, 3, 0.2)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters())
+
+    l = train(model, train_dataloader, test_dataloader, optimizer, loss_fn, epoch=20)
